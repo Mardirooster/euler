9e5d6172d48cee0e8d9a4bde54d02618fa4db112
Mardirooster <samuelmardirosian@gmail.com>
2022-07-14 09:18:10 -0400

    Add sum_of_multiples_function
    Add a basic function to calculate the sum of all multiples of a number below a maximum.

--- euler_1.py
+++ euler_1.py
@@ -1,4 +1,7 @@
 # the largest multiple of x under y = (y // x) * x
-# the sum of multiples of x can be expressed as follows:
+# any sum of sequential multiples of x can be expressed as follows:
 # 3 + 6 + 9 + ... = 3 * (1 + 2 + 3 + ...)
 # 1 + 2 + 3 + ... n is a triangular number and can be expressed as n*(n+1)/2
+def sum_of_multiples(number,max=100):
+		n = max // number
+		return ((n*(n+1)) / 2 ) * number
