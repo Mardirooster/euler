d8d3d832142bb9d2c7e9913dc99711dbd66129a3
Mardirooster <samuelmardirosian@gmail.com>
2022-07-14 09:55:57 -0400

    Add fibonacci function to euler_2
    Add a basic function that returns a list of all fibonacci numbers below a maximum.

--- euler_2.py
+++ euler_2.py
@@ -1,1 +1,12 @@
 # calculate the sum of all even fibonacci terms
+# [1,2,3,5,8,13,21,34,]
+
+
+# return a list of the fibonacci sequence up to a max
+def fibonacci(max):
+	sequence = [1,2]
+	next_number = sequence[-1] + sequence [-2]
+	while next_number <= max:
+		sequence.append(next_number)
+		next_number = sequence[-1] + sequence [-2]
+	return sequence
