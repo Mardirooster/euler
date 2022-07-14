97ad9e3ab6dd8bc5ecd68efe65eff4bb802488c9
Mardirooster <samuelmardirosian@gmail.com>
2022-07-14 09:35:36 -0400

    Correct formula in sum_of_multiples
    The largest multiple of x less than y is actually ((y - 1) // x) * x. Previously my formula was just (y//x) * x, which fails if y is a multiple of x, as y is not the largest multiple of x less than y.

--- euler_1.py
+++ euler_1.py
@@ -1,10 +1,12 @@
-# the largest multiple of x under y = (y // x) * x
+# the largest multiple of x under y = (y - 1 // x) * x
 # any sum of sequential multiples of x can be expressed as follows:
 # 3 + 6 + 9 + ... = 3 * (1 + 2 + 3 + ...)
 # 1 + 2 + 3 + ... n is a triangular number and can be expressed as n*(n+1)/2
 def sum_of_multiples(number,max=100):
-		n = max // number
-		return triangular_number * number
+    n = (max-1) // number
+    return triangular_number(n) * number
 
 def triangular_number(n):
-	return (n*(n+1)) / 2
+    return int((n*(n+1)) / 2)
+
+
