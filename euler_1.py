e0d092e1618be6d3da7998442dc7f68bdb668624
Mardirooster <samuelmardirosian@gmail.com>
2022-07-14 09:12:10 -0400

    Start Euler 1

---
+++ euler_1.py
@@ -0,0 +1,4 @@
+# the largest multiple of x under y = (y // x) * x
+# the sum of multiples of x can be expressed as follows:
+# 3 + 6 + 9 + ... = 3 * (1 + 2 + 3 + ...)
+# 1 + 2 + 3 + ... n is a triangular number and can be expressed as n*(n+1)/2
